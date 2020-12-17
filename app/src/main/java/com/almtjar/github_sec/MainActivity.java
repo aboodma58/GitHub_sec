d8262e599a0a4327d24037b11f41c02e83f66c2e
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2020-12-17 22:21:16 +0300

    Update MainActivity.java

--- app/src/main/java/com/almtjar/github_sec/MainActivity.java
+++ app/src/main/java/com/almtjar/github_sec/MainActivity.java
@@ -3,13 +3,15 @@ package com.almtjar.github_sec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Here Write Some Code
+        textView = findViewById(R.id.textView);
     }
 }
